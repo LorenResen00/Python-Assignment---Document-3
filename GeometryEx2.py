c80415927880511f6efd87a553be5ee804698f9b
Lorenzo Resendiz <150934193+LorenResen00@users.noreply.github.com>
2024-03-14 11:02:35 -0700

    Add files via upload

---
+++ GeometryEx2.py
@@ -0,0 +1,30 @@
+# Note: For Module 4, Data for python Doc 3, Exercise HW 2 - Lorenzo Resendiz
+    
+# Path for import doc: GeometryEx2.py
+
+import math
+
+def calculate_square_footage(length, width):
+    """
+    Calculates the square footage of a house.
+
+    Parameters:
+    - length (float): The length of the house.
+    - width (float): The width of the house.
+
+    Returns:
+    - float: The square footage of the house.
+    """
+    return length * width
+
+def calculate_circumference(radius):
+    """
+    Calculates the circumference of a circle.
+
+    Parameters:
+    - radius (float): The radius of the circle.
+
+    Returns:
+    - float: The circumference of the circle.
+    """
+    return 2 * math.pi * radius
